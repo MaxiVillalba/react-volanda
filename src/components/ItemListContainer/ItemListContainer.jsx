c441547792dd138aa063d12fd4ce15ba8a0bd1dd
unknown <massevillalba@gmail.com>
2024-10-29 12:41:12 +0100

    se realizan modificaciones

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,38 @@
-const ItemListContainer = ({greetings}) => {
-  return (
-    <div>{greetings}</div>
-  )
-}
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import Cards from '../Cards/Cards';
+import Carousel from '../Carousels/Carousel';
 
-export default ItemListContainer
+const ItemListContainer = ({ greetings }) => {
+  const { categoryName } = useParams();
+
+  const items = [
+    { title: 'Londres', category: 'europe', text: 'Descubre Londres', image: "https://images.unsplash.com/photo-1533929736458-ca588d08c8be?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
+    { title: 'Barcelona', category: 'europe', text: 'Descubre Barcelona', image: "https://images.unsplash.com/photo-1688680437050-d2cddf4f20b2?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    { title: 'Tokyo', category: 'asia', text: 'Descubre Tokyo', image: "https://images.unsplash.com/photo-1549693578-d683be217e58?q=80&w=1777&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
+    { title: 'Marsella', category: 'europe', text: 'Descubre Marsella', image: "https://plus.unsplash.com/premium_photo-1661963861529-02951a02a25f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+  ];
+
+  const filteredItems = categoryName ? items.filter(item => item.category === categoryName) : items;
+
+  return (
+    <div>
+      <h2>{greetings}</h2>
+      <Carousel />
+
+      <div className="card-group">
+        {filteredItems.map((item, index) => (
+          <Cards
+            key={index}
+            title={item.title}
+            text={item.text}
+            image={item.image}
+            updatedAt={item.updatedAt}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ItemListContainer;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,36 +1,43 @@
-import CartWidget from"./CartWidget"
-import "./navbar.scss"
+import CartWidget from "./CartWidget";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-<nav>
-<nav className="navbar navbar-expand-lg bg-body-tertiary">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#"></a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Destinos</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Paquetes</a>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled" aria-disabled="true">Promociones</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-  <CartWidget className="icon-cart" />
-</nav>
-</nav>
-  )
-}
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">Volanda</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/destinos">Destinos</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/category/paquetes">Paquetes</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link disabled" aria-disabled="true" to="/category/promociones">Promociones</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <CartWidget className="icon-cart" />
+    </nav>
+  );
+};
 
-export default NavBar
+export default NavBar;
